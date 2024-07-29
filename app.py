29fd40d740a9b6d07b73c732286c89df3a1f2f17
5hu8h4m/n4g4 <nagdiveshubham850@gmail.com>
2024-07-29 21:56:18 +0200

    fixed app.py not found

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import requests
 import json
 import os
 
+def load_config(config_path):
+    with open(config_path, 'r') as config_file:
+        return json.load(config_file)
+
 def upload_file(api_url, api_key, file_path):
     upload_url = f"{api_url}/upload"
     headers = {'Authorization': api_key}
@@ -13,7 +17,7 @@ def upload_file(api_url, api_key, file_path):
             files = {'file': (file_name, file, 'application/vnd.android.package-archive')}
             response = requests.post(upload_url, headers=headers, files=files)
             response.raise_for_status()
-            return response.json().get('hash')
+        return response.json().get('hash')
     except requests.exceptions.RequestException as e:
         print(f"Error uploading file: {e}")
         return None
@@ -73,8 +77,10 @@ def save_json_report(report, output_dir, filename='mob_sf_report.json'):
         print(f"Error saving JSON report: {e}")
 
 def main(file_path, api_key, api_url):
-    output_dir = os.path.join(os.path.dirname(file_path), 'output')
-    
+    if not api_url or not api_key:
+        print("API URL or API Key missing.")
+        return
+
     file_hash = upload_file(api_url, api_key, file_path)
     if file_hash:
         print(f"File uploaded successfully. Hash: {file_hash}")
@@ -82,8 +88,8 @@ def main(file_path, api_key, api_url):
             print("Scan triggered successfully.")
             report = fetch_report(api_url, api_key, file_hash)
             if report:
-                save_json_report(report, output_dir)
-                download_pdf_report(api_url, api_key, file_hash, output_dir)
+                save_json_report(report, '/output')
+                download_pdf_report(api_url, api_key, file_hash, '/output')
             else:
                 print("Failed to fetch JSON report.")
         else:
@@ -93,7 +99,7 @@ def main(file_path, api_key, api_url):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Upload APK file to MobSF, trigger scan, and fetch JSON and PDF reports.')
-    parser.add_argument('file', type=str, help='Path to the APK file')
+    parser.add_argument('file', type=str, help='Path to the APK file (located in /apk directory)')
     parser.add_argument('--api-key', required=True, type=str, help='API Key for accessing the MobSF API')
     parser.add_argument('--api-url', required=True, type=str, help='URL of the MobSF API')
     args = parser.parse_args()
